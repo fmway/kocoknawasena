b5263173e4a4dab36596976f31abfb58de771fd1
Fauzan4603 <fauzannasihin5@upi.edu>
2023-11-29 17:53:18 +0700

    Migrate to React

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className=''>
+        <nav className='bg-primary drop-shadow-xl' id='nav'>
+          <div className='flex justify-between mx-auto px-3 py-1'>
+            <div className='text-2xl font-black text-primary'>
+              <h1>KOCOK-KOCOK</h1>
+              <h1>NAWASENA</h1>
+            </div>
+            <button className='w-14 h-14 my-auto' id='profile-icon'>
+            <svg viewBox="0 0 112 110" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M0 0H112V110H0V0Z" fill="white" fillOpacity="0.01"/>
+              <path d="M42.6624 98.934C34.8431 96.6476 27.883 92.4028 22.3719 86.7783C24.4278 84.3851 25.6668 81.293 25.6668 77.9167C25.6668 70.3228 19.3988 64.1667 11.6668 64.1667C11.1991 64.1667 10.7367 64.1891 10.2808 64.2331C9.65967 61.2508 9.3335 58.1627 9.3335 55C9.3335 50.2091 10.082 45.5897 11.4703 41.2486C11.5357 41.2496 11.6012 41.25 11.6668 41.25C19.3988 41.25 25.6668 35.0939 25.6668 27.5C25.6668 25.32 25.1502 23.2584 24.2309 21.4271C29.6277 16.4986 36.2147 12.8102 43.5217 10.8236C45.8371 15.281 50.5558 18.3334 56.0002 18.3334C61.4445 18.3334 66.1632 15.281 68.4786 10.8236C75.7857 12.8102 82.3727 16.4986 87.7694 21.4271C86.8501 23.2584 86.3335 25.32 86.3335 27.5C86.3335 35.0939 92.6015 41.25 100.333 41.25C100.399 41.25 100.465 41.2496 100.53 41.2486C101.918 45.5897 102.667 50.2091 102.667 55C102.667 58.1627 102.341 61.2508 101.719 64.2331C101.264 64.1891 100.801 64.1667 100.333 64.1667C92.6015 64.1667 86.3335 70.3228 86.3335 77.9167C86.3335 81.293 87.5725 84.3851 89.6284 86.7783C84.1173 92.4028 77.1572 96.6476 69.338 98.934C67.5334 93.3896 62.2442 89.375 56.0002 89.375C49.7562 89.375 44.467 93.3896 42.6624 98.934Z" fill="#2F88FF" stroke="white" strokeWidth="10" strokeLinejoin="round"/>
+              <path d="M56.0003 71.0417C65.021 71.0417 72.3337 63.8596 72.3337 55C72.3337 46.1405 65.021 38.9584 56.0003 38.9584C46.9797 38.9584 39.667 46.1405 39.667 55C39.667 63.8596 46.9797 71.0417 56.0003 71.0417Z" fill="#43CCF8" stroke="white" strokeWidth="10" strokeLinejoin="round"/>
+            </svg>
+            </button>
+          </div>
+        </nav>
+        <section id='kocok-body' className='relative'>
+          <div className='flex flex-col container bg-secondary my-12 h-24 w-64 rounded-r-3xl justify-center text-center'>
+            <h2 className='text-redery drop-shadow-xl text-xl font-bold'>Buatlah kelompokmu</h2>
+            <h2 className='text-redery drop-shadow-xl text-xl font-bold'>dengan jujur dan adil</h2>
+          </div>
+        </section>
+      </div>
+      <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+import defaultTheme from 'tailwindcss/defaultTheme';
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#2F88FF',
+        secondary: '#E6E6E6',
+        redery: '#9D3B3B',
+      },
+      fontFamily: {
+        'sans': [ 'Poppins', 'Montserrat', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+}
